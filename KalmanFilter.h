5793402399f4fca75b9d207137fe7896ef7f8a8b
Coela Can't! <coelacannot@gmail.com>
2021-09-02 23:28:55 -0400

    Separated ESP32 code for proto controller to new codebase

---
+++ KalmanFilter.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class KalmanFilter {
+private:
+	double gain;
+	int memory;
+	double* values;
+  int currentAmount = 0;
+
+	double* ShiftArray(double arr[]);
+
+public:
+	KalmanFilter();
+	KalmanFilter(double gain, int memory);
+	double Filter(double value);
+
+};
